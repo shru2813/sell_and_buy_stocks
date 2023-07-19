096c26c52930dd5765b7f2413bf312f21d898d60
shru2813 <shrutisenthilkumaran@gmail.com>
2023-07-19 19:40:29 +0530

    INITIAL COMMIT HAS BEEN DONE

--- reverse_the_array.py
+++
@@ -1,8 +0,0 @@
-def reverseList(A, start, end):
-    while start < end:
-        A[start], A[end] = A[end], A[start]
-        start += 1
-        end -= 1
-A = list(map(int,input().split()))
-reverseList(A, 0, len(A)-1)
-print(A)
